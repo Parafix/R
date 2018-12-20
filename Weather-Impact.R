1a12b7a3fdf40c41c5d87917ad3708dc93f490cf
Dries Bultynck <dries@driesbultynck.be>
2018-12-20 18:44:55 +0100

    Update met predictie
    Nog outliers eruit halen - predictie is nu te far off van realiteit daardoor?

--- Weather-Impact.R
+++ Weather-Impact.R
@@ -7,6 +7,8 @@
 # Questions, bugs & requests: dries@driesbultynck.be or dries@parafix.io
 # References:
 # https://github.com/BvHest/KNMIr/blob/master/vignettes/HowToUseKNMIr.Rmd
+# Alternative:
+# https://github.com/ropensci/nasapower
 # ------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------
@@ -23,9 +25,21 @@ col<- colorRampPalette(c("red", "white", "blue"))(20)
 # ------------------------------------------------------------------------
 install.packages("install.load")
 library("install.load")
-install_load("devtools","googleAuthR","googleAnalyticsR","ggplot2","ggthemes","scales","plyr","lubridate","reshape2","TTR","forecast","tidyr","CausalImpact","dplyr","formatR","corrplot","Hmisc","PerformanceAnalytics")
+install_load("devtools","googleAuthR","googleAnalyticsR","ggplot2","ggthemes","scales","plyr","lubridate","reshape2","TTR","forecast","tidyr","CausalImpact","dplyr","formatR","corrplot","Hmisc","PerformanceAnalytics", "caret")
 devtools::install_github("BvHest/KNMIr", force=TRUE)
 library(KNMIr)
+
+# ------------------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------------------
+
+colnames_to_numeric <- function(df) {
+  for (i in colnames(df)){
+    colname <- colnames(df[i])
+    #if(!is.numeric(colname))
+    df[[colname]] <- as.numeric(df[[colname]])
+  }
+}
 
 # ------------------------------------------------------------------------
 # Import Weather data
@@ -70,57 +84,200 @@ names(gaData)[5]<-"addToCart"
 # Merge weather & google analytics data
 # ------------------------------------------------------------------------
 
-weatherData <- weatherData[,-c(1:2)]
+weatherData <- weatherData[,-c(1,2,11)]
 weatherData$date <- as.Date(weatherData$date)
 gaData$date <- as.Date(gaData$date)
 
 dataMerged <- merge(weatherData,gaData,by="date")
-dataMerged <- dataMerged[,-1]
-dataMerged <- dataMerged[,-c(9,11)]
-
-for (i in colnames(dataMerged)){
-  colname <- colnames(dataMerged[i])
-  #if(!is.numeric(colname))
-    dataMerged[[colname]] <- as.numeric(dataMerged[[colname]])
-}
+dataMerged <- dataMerged[,-c(10,11)]
 
 # ------------------------------------------------------------------------
 # Filter merged data
 # ------------------------------------------------------------------------
 
-
+data201808 <- dataMerged %>% filter(date > "2018-07-31", date < "2018-09-01")
+data201804t06 <- dataMerged %>% filter(date > "2018-03-31", date < "2018-07-01")
 
 # ------------------------------------------------------------------------
-# Correlation
+# Correlation total merged data
 # ------------------------------------------------------------------------
 
+#delete date
+dataMerged <- dataMerged[,-1]
+
+#make everything numeric
+dataMerged <- colnames_to_numeric(dataMerged)
+
+#correlation total period
 corMerged <- cor(dataMerged)
-corrplot(corMerged,method="number",col=col,type="upper", order="alphabet", title = "Link tussen features - 2018-01-01 tot 2018-12-01")
+corrplot(corMerged,method="number",col=col,type="upper", order="alphabet", title = "")
 
-cor.test(dataMerged$straling,dataMerged$addToCart)
-cor.test(dataMerged$straling,dataMerged$transactions)
-cor.test(dataMerged$straling,dataMerged$transactionRevenue)
+#visualize highlights in correlations to see impact
+dataMergedTransactionsPerWeek <- dataMerged %>% group_by(week) %>% summarise(transactions = sum(transactions), transactionRevenue = sum(transactionRevenue), addToCart=sum(addToCart))
+gg <- ggplot(dataMergedTransactionsPerWeek, aes(dataMergedTransactionsPerWeek$week, dataMergedTransactionsPerWeek$transactions,colour = dataMergedTransactionsPerWeek$transactionRevenue)) + geom_line(size=(dataMergedTransactionsPerWeek$transactionRevenue/50000))
+gg + labs(x = "Week", y ="Transactions", title="Trend of transactions per week", subtitle="2018", caption = "(based on Google Analytics data)", color="transactionRevenue per ???50.000")
+
+#cor.test(dataMerged$straling,dataMerged$addToCart)
+#cor.test(dataMerged$straling,dataMerged$transactions)
+#cor.test(dataMerged$straling,dataMerged$transactionRevenue)
 
 #pairs(corMerged)
 #cor.test(weatherData$percZon,weatherData$gemWind)
 #chart.Correlation(dataMerged, histogram=TRUE, pch=19)
 
+fit1 <- lm(addToCart ~ newUsers, dataMerged)
+summary(fit1)
+
+RMSE <- function(error) { sqrt(mean(error^2)) }
+RMSE(fit1$residuals)
+
+mae <- function(error) { mean(abs(error)) }
+mae(fit1$residuals)
+
+set.seed(23)
+newNewUsers = data.frame(newUsers = c(927))
+round(predict(fit1, newNewUsers , interval = 'prediction'))
+round(predict(fit1, newNewUsers , interval = 'confidence'))
+
+plot(dataMerged$addToCart,col="blue",type="l")
+lines(fit1$fitted.values,col="red")
+
+#opnieuw proberen met outliers eruit te halen!!!!!
+
+
 # ------------------------------------------------------------------------
-# Prediction
+# Correlation August 2018
+# ------------------------------------------------------------------------
+#delete date & month value as timeframe is only one month
+data201808 <- data201808[,-c(1,10)]
+
+#make everything numeric
+data201808 <- colnames_to_numeric(data201808)
+
+corData201808 <- cor(data201808)
+corrplot(corData201808,method="number",col=col,type="upper", order="alphabet", title = "")
+
+#visualize highlights in correlations to see impact
+data201808TransactionsPerWeek <- data201808 %>% group_by(week) %>% summarise(transactions = sum(transactions), transactionRevenue = sum(transactionRevenue), addToCart=sum(addToCart))
+gg <- ggplot(data201808TransactionsPerWeek, aes(data201808TransactionsPerWeek$week, data201808TransactionsPerWeek$transactions,colour = data201808TransactionsPerWeek$transactionRevenue)) + geom_line(size=(data201808TransactionsPerWeek$transactionRevenue/50000))
+gg + labs(x = "Week", y ="Transactions", title="Trend of transactions per week", subtitle="August 2018", caption = "(based on Google Analytics data)", color="transactionRevenue per ???50.000")
+#plot(data201808TransactionsPerWeek$week,data201808TransactionsPerWeek$transactions, type="l", col="blue", lwd=1, pch=13, xlab="Week", ylab="Transactions", main="Decay transactions per week")
+#lines(data201808TransactionsPerWeek$week,data201808TransactionsPerWeek$transactionRevenue,col="red")
+
+#test significance of features -> if valuable to work with
+cor.test(data201808$week,data201808$addToCart) #significant, middelmatige afwijking (22)
+cor.test(data201808$maxTemp,data201808$addToCart) #significant, grote afwijking (36)
+cor.test(data201808$gemTemp,data201808$addToCart) #significant, heel grote afwijking (40)
+cor.test(data201808$straling,data201808$addToCart) #significant, heel grote afwijking (40)
+cor.test(data201808$percZon,data201808$addToCart) #niet significant, heel grote afwijking (50)
+cor.test(data201808$newUsers,data201808$addToCart) #significant, kleine afwijking (7)
+
+# ------------------------------------------------------------------------
+# Build model - August 2018
 # ------------------------------------------------------------------------
 
-#split in train & test data
-#simpel ????nlijning model bouwen
-#test model accuraatheid
-#voorspel waarden op basis van model
+#split in train & test data ?
 
+#fit1 <- lm(data201808$addToCart ~ data201808$week, data201808)
+#fit2 <- lm(data201808$addToCart ~ data201808$week + data201808$newUsers, data201808)
+#fit3 <- lm(data201808$addToCart ~ data201808$week + data201808$maxTemp, data201808)
+#fit3 <- lm(data201808$addToCart ~ data201808$week + data201808$maxTemp + data201808$gemTemp + data201808$straling + data201808$percZon, data201808)
+#fit4 <- lm(data201808$addToCart ~ data201808$week + data201808$maxTemp + data201808$straling + data201808$percZon, data201808)
+#fit5 <- lm(data201808$addToCart ~ data201808$week + data201808$maxTemp + data201808$percZon, data201808)
+#fit6 <- lm(data201808$addToCart ~ data201808$week + data201808$maxTemp^2 + data201808$gemTemp^2 + data201808$straling^2 + data201808$percZon^2, data201808)
+
+fit1 <- lm(addToCart ~ newUsers, data201808)
+fit2 <- lm(addToCart ~ maxTemp, data201808)
+fit3 <- lm(addToCart ~ maxTemp + gemTemp + straling + percZon, data201808)
+fit4 <- lm(addToCart ~ maxTemp + straling + percZon, data201808)
+fit5 <- lm(addToCart ~ maxTemp + percZon, data201808)
+fit6 <- lm(addToCart ~ week, data201808)
+fit7 <- lm(addToCart ~ ., data201808)
+
+anova(fit1, fit2, fit3, fit4, fit5, fit6, fit7)
+summary(fit3)
+
+RMSE <- function(error) { sqrt(mean(error^2)) }
+RMSE(fit7$residuals)
+
+mae <- function(error) { mean(abs(error)) }
+mae(fit7$residuals)
+
+#confint(fit2, level=0.95)
+#coefficients(fit1)
+#fitted(fit1)
+#residuals(fit1)
+#vcov(fit1)
+#influence(fit1)
+#plot(fit1)
+
+# ------------------------------------------------------------------------
+# Prediction - August 2018
+# ------------------------------------------------------------------------
+
+set.seed(3)
+#unique_clicks.c = scale(fbDataAdsetsLINK_CLICKS$unique_clicks, center=TRUE, scale=TRUE)
+#fit1$residuals
+#fit1$fitted.values
+
+newWeek = data.frame(week = c(31,32,33,34,35))
+predict(fit6, newWeek , interval = 'prediction')
+predict(fit6, newWeek , interval = 'confidence')
+prediction <- round(predict(fit6, newWeek, interval = "confidence"))
+
+data201808TransactionsPerWeek$addToCart
+dfPrediction <- as.data.frame(prediction)
+dfPrediction$week <- index(dfPrediction)
+dfPrediction <- colnames_to_numeric(dfPrediction)
+dfPrediction$currentAddToCart <- data201808TransactionsPerWeek$addToCart 
+
+ggplot(data201808TransactionsPerWeek, aes(x=week, y=addToCart)) + geom_point(color="red", size = 3) + geom_smooth(method=lm, color="blue")
+
+ggplot(dfPrediction, aes(x=week, y=prediction)) + geom_point(color="red", size = 3) + geom_smooth(method=lm, color="blue")
+
+gg <- ggplot(dfPrediction) + 
+  theme_minimal() + 
+  ggtitle("Predicted (red) vs Current (blue)")
+gg + geom_line(aes(x = week, y = prediction), col = "red") + geom_line(aes(x = week, y = currentAddToCart), col = "blue")
+
+
+newNewUsers = data.frame(newUsers = c(3204,3023,888, 999, 0))
+#prediction <- round(predict(fit1, newNewUsers, interval = "prediction"))
+prediction <- round(predict(fit1, newNewUsers))
+prediction
+
+# ------------------------------------------------------------------------
+# Correlation April - Mei - Juni 2018
+# ------------------------------------------------------------------------
+
+data201804t06 <- data201804t06[,-c(1,10)]
+
+#make everything numeric
+data201804t06 <- colnames_to_numeric(data201804t06)
+
+corData201804t06 <- cor(data201804t06)
+corrplot(corData201804t06,method="number",col=col,type="upper", order="alphabet", title = "")
+
+data201804t06TransactionsPerWeek <- data201804t06 %>% group_by(week) %>% summarise(transactions = sum(transactions), transactionRevenue = sum(transactionRevenue), addToCart=sum(addToCart))
+gg <- ggplot(data201804t06TransactionsPerWeek, aes(data201804t06TransactionsPerWeek$week, data201804t06TransactionsPerWeek$transactions,colour = data201804t06TransactionsPerWeek$transactionRevenue)) + geom_line(size=(data201804t06TransactionsPerWeek$transactionRevenue/50000))
+gg + labs(x = "Week", y ="Transactions", title="Trend of transactions per week", subtitle="2018", caption = "(based on Google Analytics data)", color="transactionRevenue per ???50.000")
+
+cor.test(data201804t06$newUsers,data201804t06$addToCart)
+cor.test(data201804t06$transactions,data201804t06$addToCart)
+
+fit1 <- lm(addToCart ~ newUsers, data201804t06)
+summary(fit1)
+fit2 <- lm(transactions ~ addToCart, data201804t06)
+summary(fit2)
+
+set.seed(3)
+prediction <- round(predict(fit2, data.frame(addToCart = c(500, 3000, 5000))))
 
 # ------------------------------------------------------------------------
 # Forecasting scenario's - extrapolation (per dag dat het straalt ...)
 # x dagen in januari
 # Forecasting timeseries -> welk weer kan je verwachten?
 # ------------------------------------------------------------------------
-
 
 #Prediction is the more general statement. A prediction (predicted value) is an expectation for a combination of predictors (predictor values). The prediction is calculated based on a model, and the model is usually obtained by a fit to some available data.
 #The most simple example is the mean of a sample. The model is most simple - there is no particular predictor involved (you only have some values for some response variable but you do not have any information on what these values may depend). The mean is the model that maximizes the likilihood (of the data under the model). It gives the expected value for the response. This means: if you had to make a guess abut any new value that is obtained in a similar way how the sample was obtained, the sample mean is your best guess.
