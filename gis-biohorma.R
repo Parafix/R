a6548615b098c0d7035d31ebe5ae7e227f25b33d
Dries Bultynck <dries@driesbultynck.be>
2019-04-02 17:37:50 +0200

    Add files via upload

--- gis-biohorma.R
+++ gis-biohorma.R
@@ -46,14 +46,36 @@ gaClient <- "AVogel"
 gaViewName <- "Master"
 gaViewId <- "2897692"
 gaDateRange <- c("2018-01-01","2018-12-31")
-gaDimensions <- c("month","city","latitude","longitude") #,"deviceCategory", sourceMedium, pagePath, month, year
-gaMetrics <- c("sessions","percentNewSessions","pageviews","entrances","exitRate","exits","bounces","bounceRate","pageValue","transactions") #exitPagePath, sessions, newUsers",goal2Completions,transactions
+gaDimensions <- c("month","city","country","longitude","latitude") #,"deviceCategory", sourceMedium, pagePath, month, year
+gaMetrics <- c("itemRevenue") 
+#for visit stuff -> "sessions","percentNewSessions","pageviews","entrances","exitRate","exits","bounces","bounceRate","pageValue","transactions"
 gaDelta <- order_type("date","ASCENDING", "DELTA")
-#gaDimFilterSourceMedium <- dim_filter("sourceMedium","REGEXP","organic|direct")
-#gaDimFilters <- filter_clause_ga4(list(gaDimFilterSourceMedium))
-gaMetFilterTransactions <- met_filter("transactions", "GREATER", 0)
-gaMetFilters <- filter_clause_ga4(list(gaMetFilterTransactions))
+gaDimFilterCountry <- dim_filter("country","EXACT","Belgium")
+gaDimFilterProducts <- dim_filter("productName","REGEXP","Echinaforce|Dormeasan|Oogdruppels|Herbamare"))
+gaDimFilters <- filter_clause_ga4(list(gaDimFilterCountry,gaDimFilterProducts))
 
+gaMetFilterItemRevenue <- met_filter("itemRevenue", "GREATER", 0)
+gaMetFilters <- filter_clause_ga4(list(gaMetFilterItemRevenue)) #gaMetFilterTransactions
+#gaMetFilterTransactions <- met_filter("transactions", "GREATER", 0) #for visit stuff
+
+filter_d <- filter_clause_ga4(list(
+  dim_filter("country", "EXACT", "Belgium"),
+  dim_filter("productName", "REGEXP", 'Hemoclin|Multi-gyn|Aciforce') #Echinaforce|Dormeasan|Oogdruppels|Herbamare
+),operator = "AND"
+)
+
+filter_m <- filter_clause_ga4(list(
+  met_filter("itemRevenue", "GREATER", 0)
+)
+)
+
+decAfterComma <- 3
+
+#list of focus products
+#list of other products
+#make weights + buckets in quantiles for better plots
+#plots
+#exports in excel
 
 # ------------------------------------------------------------------------
 # THEMES & PALETTES
@@ -99,29 +121,65 @@ col_wijs_r = c("#FF4040")
 #Google Analytics
 ga_auth(new_user = TRUE)
 #meta <- google_analytics_meta()
-gaData <- google_analytics(gaViewId, date_range = gaDateRange, metrics = gaMetrics, dimensions = gaDimensions, met_filters = gaMetFilters, anti_sample = TRUE)
+#gaData <- google_analytics(gaViewId, date_range = gaDateRange, metrics = gaMetrics, dimensions = gaDimensions, dim_filters = gaDimFilters , met_filters = gaMetFilters, anti_sample = TRUE)
+gaData <- google_analytics(gaViewId, date_range = gaDateRange, metrics = gaMetrics, dimensions = gaDimensions, dim_filters = filter_d , met_filters = filter_m, anti_sample = TRUE)
 
 #Excel
+#dfPABWL = read.xlsx("2018-postcodes-of-Wallonian-pharmacists.xlsx")
+#dfPABWLE = read.xlsx("2018-postcodes-of-Wallonian-pharmacists-Echinaforce.xlsx")
 dfBelgium = read.csv("zipcode-belgium.csv", header = FALSE)
-dfPABWL = read.xlsx("2018-postcodes-of-Wallonian-pharmacists.xlsx")
-dfPABWLE = read.xlsx("2018-postcodes-of-Wallonian-pharmacists-Echinaforce.xlsx")
 dfPAB = read.xlsx("2018-postcodes-of-biorhorma-pharmacists.xlsx")
 
 
 # ------------------------------------------------------------------------
 # DATA HANDLING
 # ------------------------------------------------------------------------
+#dfPABWLE[is.na(dfPABWLE)] <- 0
+#dfPABWL[is.na(dfPABWL)] <- 0
 
 dfPAB$postcode <- as.numeric(dfPAB$postcode)
-
-dfPABWLE[is.na(dfPABWLE)] <- 0
-dfPABWL[is.na(dfPABWL)] <- 0
 dfPAB[is.na(dfPAB)] <- 0
 
-dfLocations <- dfBelgium %>% select(V2,V1,V3,V4) %>% filter(V1 %in% dfPAB$postcode)
+dfLocations <- dfBelgium %>% dplyr::select(V2,V1,V3,V4) %>% filter(V1 %in% dfPAB$postcode)
 colnames(dfPAB)[1] <- "V1"
-temp <- inner_join(dfLocations,dfPAB, by=c("V1"))
+dfOffline <- inner_join(dfLocations,dfPAB, by=c("V1"))
 
+#visit stuff
+gaCities <- gaData %>%
+  filter(country == "Belgium") %>%
+  group_by(city) %>%
+  summarise(longitude = max(longitude), latitude= max(latitude), sessions = sum(sessions), pageviews=sum(pageviews), entrances = sum(entrances), bounces = sum(bounces), pageValue = mean(pageValue), transactions=sum(transactions, percentNewSessions =  mean(percentNewSessions), exitRate = mean(exitRate), bounceRate=mean(bounceRate))) %>%
+  filter(city!="(not set)") %>%
+  mutate(
+    quartile = ntile(sessions, 4),
+    decentile = ntile(sessions, 10),
+    percentile = ntile(sessions, 100),
+    percent = sessions/sum(sessions)
+  )
+
+#itemRev stuff
+gaCities <- gaData %>%
+  filter(country == "Belgium") %>%
+  group_by(city) %>%
+  summarise(longitude = max(longitude), latitude= max(latitude), itemRevenue = sum(itemRevenue)) %>%
+  filter(city!="(not set)") %>% 
+  mutate(
+    quartile = ntile(itemRevenue, 4),
+    decentile = ntile(itemRevenue, 10),
+    percentile = ntile(itemRevenue, 100),
+    percent = itemRevenue/sum(itemRevenue),
+    addUp = cumsum(percent)
+    )
+
+dfOnline <- as.data.frame(gaCities)
+colnames(dfOnline)[2] <- "V3"
+colnames(dfOnline)[3] <- "V4"
+dfOnline$V3 <- as.numeric(dfOnline$V3)
+dfOnline$V4 <- as.numeric(dfOnline$V4)
+
+#joinen kan niet omwillen van verschillende long & lat cijfers!
+#GA long & lat niet zelfde als GIS
+#multichannel <- inner_join(offline,gaCities,by=c("V3","V4"))
 
 # ------------------------------------------------------------------------
 # VISUALIZATION
@@ -151,53 +209,60 @@ temp <- inner_join(dfLocations,dfPAB, by=c("V1"))
 #plot locations
 qmplot(V3,V4,data=dfLocations, maptype = "toner-background", color = I("red"), geom = "density2d")
 
-#plot density of biohorma pharmacists
-qmplot(V3,V4,data=temp, geom = "blank", zoom = 9, maptype = "toner-lite", legend = "topleft") + 
+#plot density of biohorma pharmacists (based on cities)
+qmplot(V3,V4,data=dfOffline, geom = "blank", zoom = 9, maptype = "toner-lite", legend = "topleft") + 
   stat_density_2d(aes(fill = ..level..), geom = "polygon", alpha = .3, color = NA) + 
   scale_fill_gradient2("Biohorma Propensity", low = "white", mid = "yellow", high = "red", midpoint = 0)
 
 #plot density of orders by biohorma pharmacists
-qmplot(V3,V4,data=temp, maptype = "toner", color=I("red2"), geom = "density2d")
-qmplot(V3,V4,data=temp, maptype = "toner", color=I("red2"), geom = "density2d") + facet_wrap(~ pharmacists) + theme(panel.spacing = unit(1, "lines"))
-qmplot(V3,V4,data=temp, maptype = "toner", color=I("red2"), size=pharmacists, zoom=10) + facet_wrap(~ pharmacists) + theme(panel.spacing = unit(1, "lines"))
+qmplot(V3,V4,data=dfOffline, maptype = "toner", color=I("red2"), geom = "density2d")
+qmplot(V3,V4,data=dfOffline, maptype = "toner", color=I("red2"), geom = "density2d") + facet_wrap(~ pharmacists) + theme(panel.spacing = unit(1, "lines"))
+qmplot(V3,V4,data=dfOffline, maptype = "toner", color=I("red2"), size=pharmacists, zoom=10) + facet_wrap(~ pharmacists) + theme(panel.spacing = unit(1, "lines"))
 
-
-
-
-tempZonderO <- temp %>% filter(Bestelden > 0)
-qmplot(V3,V4,data=tempZonderO, maptype = "toner", color=Bestelden, size=Bestelden.Echinaforce) 
-qmplot(V3,V4,data=tempZonderO, maptype = "toner", color=Bestelden, size=Bestelden.Echinaforce) + facet_wrap(~ Bestelden.Echinaforce) + theme(panel.spacing = unit(1, "lines"))
-qmplot(V3,V4,data=tempZonderO, maptype = "toner", color=I("red2"), geom = "density2d")
-qmplot(V3,V4,data=tempZonderO, maptype = "toner", color=I("red2"), geom = "density2d") + facet_wrap(~ Bestelden.Echinaforce) + theme(panel.spacing = unit(1, "lines"))
-  
-qmplot(V3,V4,data=temp, maptype = "toner-background", color = I("red"), geom = "density2d") + facet_wrap(~ Bestelden.Echinaforce) + theme(panel.spacing = unit(1, "lines"))
-qmplot(V3,V4,data=temp, maptype = "toner-background", color = Bestelden) + facet_wrap(~ Bestelden.Echinaforce)
-qmplot(V3,V4,data=temp, maptype = "toner-background", color = Bestelden.Echinaforce, Zoom=6) + facet_wrap(~ Bestelden.Echinaforce) + theme(panel.spacing = unit(1, "lines"))
-
-
-#robberies <- violent_crimes %>% filter(offense == "robbery")
-tempEnkelBestelden <- temp %>% filter(Bestelden > 0)
-tempEnkelBestelden <- tempEnkelBestelden[,-c(6,7)]
-qmplot(V3,V4,data=tempEnkelBestelden, geom = "blank", zoom = 9, maptype = "toner-lite", legend = "topleft") + 
+#plot density of biohorma visitors (based on cities)
+qmplot(V3,V4,data=dfOnline, geom = "blank", zoom = 9, maptype = "toner-lite", legend = "topleft") + 
   stat_density_2d(aes(fill = ..level..), geom = "polygon", alpha = .3, color = NA) + 
   scale_fill_gradient2("Biohorma Propensity", low = "white", mid = "yellow", high = "red", midpoint = 0)
 
-tempBezocht <- temp %>% filter(Bezocht > 0)
-tempBezocht <-tempBezocht[,-c(5,7)] 
-qmplot(V3,V4,data=tempBezocht, geom = "blank", zoom = 9, maptype = "toner-lite", legend = "topleft") + 
-  stat_density_2d(aes(fill = ..level..), geom = "polygon", alpha = .3, color = NA) + 
-  scale_fill_gradient2("Biohorma Bezocht Propensity", low = "white", mid = "yellow", high = "red", midpoint = 0)
+#plot density of sessions by visitors avogel.be
+qmplot(V3,V4,data=dfOnline, maptype = "toner", color=I("red2"), geom = "density2d")
+qmplot(V3,V4,data=dfOnline, maptype = "toner", color=I("red2"), geom = "density2d") + facet_wrap(~ decentile) + theme(panel.spacing = unit(1, "lines"))
+qmplot(V3,V4,data=dfOnline, maptype = "toner", color=I("red2"), size=itemRevenue, zoom=8) + facet_wrap(~ decentile) + theme(panel.spacing = unit(1, "lines"))
 
-tempZonder0Echinaforce <- temp %>% filter(Bestelden.Echinaforce > 0)
-qmplot(V3,V4,data=tempZonder0Echinaforce, geom = "blank", zoom = 9, maptype = "toner-lite", legend = "topleft") + 
-  stat_density_2d(aes(fill = ..level..), geom = "polygon", alpha = .3, color = NA) + 
-  scale_fill_gradient2("Echinaforce Propensity", low = "white", mid = "yellow", high = "red", midpoint = 0)
 
-tempEnkelEchinaforce <- temp %>% filter(Bestelden.Echinaforce > 0)
-tempEnkelEchinaforce <- tempEnkelEchinaforce[,-c(5,6)]
-qmplot(V3,V4,data=tempEnkelEchinaforce, geom = "blank", zoom = 9, maptype = "toner-lite", legend = "topleft") + 
-  stat_density_2d(aes(fill = ..level..), geom = "polygon", alpha = .3, color = NA) + 
-  scale_fill_gradient2("Echinaforce Propensity", low = "white", mid = "yellow", high = "red", midpoint = 0)
+# tempZonderO <- temp %>% filter(Bestelden > 0)
+# qmplot(V3,V4,data=tempZonderO, maptype = "toner", color=Bestelden, size=Bestelden.Echinaforce) 
+# qmplot(V3,V4,data=tempZonderO, maptype = "toner", color=Bestelden, size=Bestelden.Echinaforce) + facet_wrap(~ Bestelden.Echinaforce) + theme(panel.spacing = unit(1, "lines"))
+# qmplot(V3,V4,data=tempZonderO, maptype = "toner", color=I("red2"), geom = "density2d")
+# qmplot(V3,V4,data=tempZonderO, maptype = "toner", color=I("red2"), geom = "density2d") + facet_wrap(~ Bestelden.Echinaforce) + theme(panel.spacing = unit(1, "lines"))
+#   
+# qmplot(V3,V4,data=temp, maptype = "toner-background", color = I("red"), geom = "density2d") + facet_wrap(~ Bestelden.Echinaforce) + theme(panel.spacing = unit(1, "lines"))
+# qmplot(V3,V4,data=temp, maptype = "toner-background", color = Bestelden) + facet_wrap(~ Bestelden.Echinaforce)
+# qmplot(V3,V4,data=temp, maptype = "toner-background", color = Bestelden.Echinaforce, Zoom=6) + facet_wrap(~ Bestelden.Echinaforce) + theme(panel.spacing = unit(1, "lines"))
+
+#robberies <- violent_crimes %>% filter(offense == "robbery")
+# tempEnkelBestelden <- temp %>% filter(Bestelden > 0)
+# tempEnkelBestelden <- tempEnkelBestelden[,-c(6,7)]
+# qmplot(V3,V4,data=tempEnkelBestelden, geom = "blank", zoom = 9, maptype = "toner-lite", legend = "topleft") + 
+#   stat_density_2d(aes(fill = ..level..), geom = "polygon", alpha = .3, color = NA) + 
+#   scale_fill_gradient2("Biohorma Propensity", low = "white", mid = "yellow", high = "red", midpoint = 0)
+# 
+# tempBezocht <- temp %>% filter(Bezocht > 0)
+# tempBezocht <-tempBezocht[,-c(5,7)] 
+# qmplot(V3,V4,data=tempBezocht, geom = "blank", zoom = 9, maptype = "toner-lite", legend = "topleft") + 
+#   stat_density_2d(aes(fill = ..level..), geom = "polygon", alpha = .3, color = NA) + 
+#   scale_fill_gradient2("Biohorma Bezocht Propensity", low = "white", mid = "yellow", high = "red", midpoint = 0)
+# 
+# tempZonder0Echinaforce <- temp %>% filter(Bestelden.Echinaforce > 0)
+# qmplot(V3,V4,data=tempZonder0Echinaforce, geom = "blank", zoom = 9, maptype = "toner-lite", legend = "topleft") + 
+#   stat_density_2d(aes(fill = ..level..), geom = "polygon", alpha = .3, color = NA) + 
+#   scale_fill_gradient2("Echinaforce Propensity", low = "white", mid = "yellow", high = "red", midpoint = 0)
+# 
+# tempEnkelEchinaforce <- temp %>% filter(Bestelden.Echinaforce > 0)
+# tempEnkelEchinaforce <- tempEnkelEchinaforce[,-c(5,6)]
+# qmplot(V3,V4,data=tempEnkelEchinaforce, geom = "blank", zoom = 9, maptype = "toner-lite", legend = "topleft") + 
+#   stat_density_2d(aes(fill = ..level..), geom = "polygon", alpha = .3, color = NA) + 
+#   scale_fill_gradient2("Echinaforce Propensity", low = "white", mid = "yellow", high = "red", midpoint = 0)
 
 
 #register_google(key = "AIzaSyBd8_0R0g41ZeHlh4KL71IYFXV1xqzuxKg", account_type = "free", day_limit = 2500)
@@ -222,12 +287,10 @@ qmplot(V3,V4,data=tempEnkelEchinaforce, geom = "blank", zoom = 9, maptype = "ton
 #geocodeQueryCheck(userType = "free")
 #str(geocode("Baylor University", output = "all"))
 
-
-
 # ------------------------------------------------------------------------
 # EXPORT
 # ------------------------------------------------------------------------
 
-#write.xlsx(dfMerged, paste(gaClient,"-",gaViewName,"-SE0-Pages-",gaDateRange[1],"-",gaDateRange[2],".xls", sep=""))
+write.xlsx(dfOnline, paste(gaClient,"-",gaViewName,"-GIS-TABOO-",gaDateRange[1],"-",gaDateRange[2],".xls", sep=""))
 #dev.copy(png, paste(gaClient,"-",gaViewName,"-SE0-Plot-Cluster-",firstPrio,".png", sep=""),width=2048,height=1536,res=72)
 #dev.off()
